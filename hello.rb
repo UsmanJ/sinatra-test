303de6cdde5a0d809bb2524aa2df7e8b329a9e2b
Lucy Fang <lucyfang2@gmail.com>
2015-09-14 11:55:36 +0100

    Added kitty picture

--- hello.rb
+++ hello.rb
@@ -1,9 +1,8 @@
 require 'sinatra'
 
 get '/' do
-  'hello!'
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+    </div>"
 end
 
-get '/secret' do
-  'This is a secret page and we installed the shotgun gem'
-end
